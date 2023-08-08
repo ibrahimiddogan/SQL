b942eefa6189943f66a3c1f5bf092f9c68228f6a
ibrahimiddogan <ibrahim.id.dogan@gmail.com>
2023-08-08 14:50:56 +0300

    Create 3.Odev.sql

---
+++ 3.Odev.sql
@@ -0,0 +1,6 @@
+select country from country where country like 'A%a';
+select country from country where country like '_____n'
+SELECT * FROM film WHERE LOWER(title) LIKE '%t%' OR UPPER(title) LIKE '%T%' LIMIT 4;
+select * from film where title like 'c&' and length>90 and rental_rate=2.99; 
+
+ 
