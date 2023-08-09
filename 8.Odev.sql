694f0f8c7efc079d5f4d0bacfb3832700ff4eb8f
ibrahimiddogan <ibrahim.id.dogan@gmail.com>
2023-08-09 18:42:42 +0300

    Create 8.Odev.sql

---
+++ 8.Odev.sql
@@ -0,0 +1,8 @@
+update employee set name = 'Alonso' where name = 'Muire';
+update employee set id = 5 where name = 'Muire';
+update employee set birtday = '2012-09-12' where name = 'Muire';
+update employee set email = 'yenimail@gmail.com' where name = 'Muire';
+delete from employee where name = 'Alonso';
+delete from employee where id = 8;
+delete from employee where birtday = '1932-10-10';
+delete from employee where email = 'adunk1@time.com';
