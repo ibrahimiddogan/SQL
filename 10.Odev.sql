56baae34fcdb8dc784089d1786f8435661bb4075
ibrahimiddogan <ibrahim.id.dogan@gmail.com>
2023-08-10 16:55:25 +0300

    Create 10.Odev.sql

---
+++ 10.Odev.sql
@@ -0,0 +1,3 @@
+select city,country from city left join country on country.country_id =city.country_id;
+select payment_id , first_name ,last_name from customer left join payment on payment.customer_id=customer.customer_id
+select rental_id , first_name ,last_name from customer full join rental on rental.customer_id=customer.customer_id
